da26c8edda25e1c00c81bcdc09f46c3a9f855f24
Jackie Chen <jackiechen2022@gmail.com>
2021-04-03 21:43:28 -0600

    1.0 stable release

--- src/com/joker/Main.java
+++ src/com/joker/Main.java
@@ -8,7 +8,7 @@ import java.io.PrintStream;
 public class Main {
 
     // Program version
-    public static final String version =  "1.0 - Release Candidate 1";
+    public static final String version =  "1.0";
 
     // Create directories for the application
     public static final File dataDir = new File("salesAppData/");
@@ -20,13 +20,6 @@ public class Main {
 
     // Print console output to log file
     private static PrintStream out;
-    static {
-        try {
-            out = new PrintStream(new FileOutputStream("salesApp_log.txt"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void programInfo() {
         System.out.println("SalesApp Version: " + version);
@@ -34,6 +27,11 @@ public class Main {
 
     public static void main(String[] args) {
         if (logging) {
+            try {
+                out = new PrintStream(new FileOutputStream("salesApp_log.txt"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
             System.setOut(out);
         }
         programInfo();
